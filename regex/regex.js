b6d3de7305411a4e9d4b41df4d96927d62fab6da
Patrice976 <catinatp@gmail.com>
2024-11-15 12:33:57 +0100

    récupérer les caractères spéciaux uniquement

--- regex/regex.js
+++ regex/regex.js
@@ -1,4 +1,4 @@
 const str = "J'utilise les expressions regulière pour retrouver des schémas de text au sein d'une chaine de caractères."
 
-const exp = /\w/g
+const exp = /[^\w\s]/g
 console.log(str.match(exp).length)
