db44092671555a971973f919b996227065792e3e
Patrice976 <catinatp@gmail.com>
2024-11-15 12:05:50 +0100

    récupérer toutes les lettres de la string

--- regex/regex.js
+++ regex/regex.js
@@ -1,4 +1,4 @@
 const str = "J'utilise les expressions regulière pour retrouver des schémas de text au sein d'une chaine de caractères."
 
-const exp = /(?:de)|(?:le)/g
+const exp = /\w/g
 console.log(str.match(exp).length)
