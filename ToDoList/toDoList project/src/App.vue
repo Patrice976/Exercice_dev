84e1a32ce9e2d46cd1a3cc0f3f639e37379432a9
Patrice976 <catinatp@gmail.com>
2025-02-24 14:19:02 +0100

    début exercie todo vue.js

---
+++ ToDoList/toDoList project/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue'
+let tasks = ref([])
+let task = ref()
+
+function addTask() {
+  tasks.value.push(task.value)
+  task.value = ''
+}
+</script>
+
+<template>
+  <p v-if="tasks.length === 0">Il n'y a aucune tâche à réaliser</p>
+  <p>{{ tasks }}</p>
+
+  <form @submit.prevent="handleSubmit">
+    <input type="text" v-model="task" placeholder="Inscrivez la tâche à ajouter dans la toDo" />
+    <button @click="addTask()">Ajouter</button>
+  </form>
+</template>
+
+<style scoped></style>
