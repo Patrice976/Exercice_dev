c640e0faa104d2e3e94becefdf4bcdeed9da705b
Patrice976 <catinatp@gmail.com>
2025-03-11 09:36:27 +0100

    création objet à push dans tableau pour gérer gestion de l'état checked

--- ToDoList/toDoList project/src/App.vue
+++ ToDoList/toDoList project/src/App.vue
@@ -4,18 +4,23 @@ let tasks = ref([])
 let task = ref()
 
 function addTask() {
-  tasks.value.push(task.value)
+  tasks.value.push({
+    title : task.value,
+  completed: false,
+  date : Date.now(),})
   task.value = ''
 }
 </script>
 
 <template>
   <p v-if="tasks.length === 0">Il n'y a aucune tâche à réaliser</p>
+  <div v-else>
   <ul>
-    <li v-for="(toDo,index) in tasks" :key="index">
-      <input type="checkbox" /><span class="toDo">{{ toDo }}</span>
+    <li v-for="(toDo,index) in tasks" :key="index" :class ="{completed:toDo.completed}">
+      <input type="checkbox" v-model ="toDo.completed"   /><span>{{ toDo.title }}</span>
     </li>
   </ul>
+  </div>
 
   <form @submit.prevent="handleSubmit">
     <input type="text" v-model="task" placeholder="Inscrivez la tâche à ajouter dans la toDo" />
@@ -26,4 +31,10 @@ function addTask() {
 <style scoped>
 .toDo {
   margin : 1rem
+}
+.completed {
+  opacity : .5 ;
+  text-decoration: line-through;
 }</style>
+
+
